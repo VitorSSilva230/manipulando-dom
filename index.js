0aa7cbeb27c8fac9b980d4e9d85e86d16cc16467
vitorSSilva230 <vitorssilva230@gmail.com>
2023-06-27 17:09:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let imgLight = "img/desenvolvedor.png";
+let imgDark = "img/desenvolvedor_dark.png";
+
+let ballLight = "img/circulo.png" ;
+let ballDark = "img/circulo_dark.png";
+
+let lightSectionColor = "#DEE2E5";
+let darkSectionColor = "red";
+
+function trocarTema() {
+    
+    document.getElementById("dev").src = imgDark;
+    let trocaDev = imgLight;
+    imgLight = imgDark;
+    imgDark = trocaDev;
+
+
+    document.getElementById("ball").src = ballDark;
+    let trocaBall = ballLight;
+    ballLight = ballDark;
+    ballDark = trocaBall;
+
+    document.getElementById("intro").style.backgroundColor = darkSectionColor;
+
+    let trocarSecton = lightSectionColor;
+    lightSectionColor = darkSectionColor;
+    darkSectionColor = trocarSecton;
+
+}
