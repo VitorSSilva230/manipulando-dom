03441d212d7246fe17a04e5cd97028296bc25508
vitorSSilva230 <vitorssilva230@gmail.com>
2023-06-28 00:14:23 -0300

    add logo

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-let imgLight = "img/desenvolvedor.png";
+let imgLight = "img/desenvolvedor_light.png";
 let imgDark = "img/desenvolvedor_dark.png";
 
-let ballLight = "img/circulo.png" ;
+let ballLight = "img/circulo_light.png" ;
 let ballDark = "img/circulo_dark.png";
 
 let lightSectionColor = "#DEE2E5";
 let darkSectionColor = "blue";
 
+let logoLight = "img/lobo_light.png";
+let logoDark = "img/lobo_dark.png";
+
+let headerLight = "white";
+let headerDark = "black";
+
+let titleLight = "white";
+let titleDark = "black";
+
+let descriptionLight = "white";
+let descriptionDark = "black";
+
 function trocarTema() {
     
-    document.getElementById("dev").src = imgDark;
-    let trocaDev = imgLight;
-    imgLight = imgDark;
-    imgDark = trocaDev;
+    document.getElementById("dev").src = imgLight;
+    let trocaDev = imgDark;
+    imgDark = imgLight;
+    imgLight = trocaDev;
 
 
-    document.getElementById("ball").src = ballDark;
-    let trocaBall = ballLight;
-    ballLight = ballDark;
-    ballDark = trocaBall;
+    document.getElementById("ball").src = ballLight;
+    let trocaBall = ballDark;
+    ballDark = ballLight;
+    ballLight = trocaBall;
 
     document.getElementById("intro").style.backgroundColor = darkSectionColor;
-
     let trocarSecton = lightSectionColor;
     lightSectionColor = darkSectionColor;
     darkSectionColor = trocarSecton;
+
+    document.getElementById("logo").src = logoDark;
+    let trocaLogo = logoLight;
+    logoLight = logoDark;
+    logoDark = trocaLogo;
+
+    document.getElementById("header").style.backgroundColor= headerLight;
+    let trocaHeader = headerDark;
+    headerDark = headerLight;
+    headerLight = trocaHeader;
+
+    document.getElementById("title").style.color= titleLight;
+    let trocaTitle = titleDark;
+    titleDark = titleLight;
+    titleLight = trocaTitle;
+
+    document.getElementById("description").style.color= descriptionLight;
+    let trocaDescription = descriptionDark;
+    descriptionDark = descriptionLight;
+    descriptionLight = trocaDescription;
+
 
 }
